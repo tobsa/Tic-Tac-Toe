c9b1a4d021e32ef6d66a6c686f0e262f5a738cf1
Tobias Savinainen <tobias@savinainen.se>
2014-01-15 00:20:16 +0100

    Player implemented

---
+++ src/GameSystem/Player.java
@@ -0,0 +1,21 @@
+package GameSystem;
+
+public abstract class Player {
+    private int id;
+    private String name;
+    
+    public Player(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+    public int getID() {
+        return id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public abstract int computeMove() throws InterruptedException;
+}
